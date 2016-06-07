38a51618c171155dadb543968d2b71f6f9877c27
Kareem <kareem.elshahawe@gmail.com>
2016-06-08 06:27:13 +0800

    Fix eslint issues

--- producer.js
+++ producer.js
@@ -1,31 +1,30 @@
-var BS_BROKER_HOST="127.0.0.1";
-var BS_BROKER_PORT="11300";
-var TUBE="test_tube";
+'use strict';
+const BS_BROKER_HOST = '127.0.0.1';
+const BS_BROKER_PORT = '11300';
+const TUBE = 'test_tube';
 
-var fivebeans = require('fivebeans');
-var client = new fivebeans.client(BS_BROKER_HOST,BS_BROKER_PORT);
+let fivebeans = require('fivebeans');
+let client = new fivebeans.client(BS_BROKER_HOST, BS_BROKER_PORT);
 
-client.on('connect',function(){
-  console.log("Connected");
-}).on('error',function(err){
-  console.log("Error: ",err);
-}).on('close',function(){
-  console.log("Closed");
-}).connect();
-
-client.use(TUBE,function(err,TUBE){
-  console.log(TUBE," is currently being used");
+client.on('connect', function () {
+	console.log('Connected');
 })
+	.on('error', function (err) {
+		console.log('Error: ', err);
+	})
+	.on('close', function () {
+		console.log('Closed');
+	})
+	.connect();
 
-client.put(3,1,5,"{msg:hellow from producer :)}",function(err,jobID){
-  if(err){
-    console.log(err);
-  }else{
-    console.log("Job created with ID: ",jobID);
-  }
+client.use(TUBE, function (err, tube) {
+	console.log(tube, ' is currently being used');
 });
 
-//
-// function submitJob(payload,priority){
-// client.put()
-// }
+client.put(3, 1, 5, '{msg:hellow from producer :)}', function (err, jobID) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('Job created with ID: ', jobID);
+	}
+});
